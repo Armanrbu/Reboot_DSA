10180d591fa8e91f2a9dc341f005c9813dfbcc61
Armanrbu <24rezams@rbunagpur.in>
2025-12-30 14:53:56 +0530

    Added logic to print remaining digit

--- extraction_digit.py
+++ extraction_digit.py
@@ -1,5 +1,9 @@
 N = 192
 
-a = N % 10
+# a = N % 10
+# print("last digit",a)           Divide by 10 then Print the remainder to find the last digit
 
-print("last digit",a)
+# b = N // 10
+# print("Remaining digit",b)         Using floor division Divide it by 10
+
+
