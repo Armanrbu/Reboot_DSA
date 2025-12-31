1feb4bb6715b3a4d9414f85096cd4e4293568003
Armanrbu <24rezams@rbunagpur.in>
2025-12-31 23:14:11 +0530

    if we want to reverse from specific index

--- reverse_arr.py
+++ reverse_arr.py
@@ -14,3 +14,18 @@ a = recursivearr(arr= [1,2,3,4,7,9,0])
 
 print(a)
 
+# if we want to reverse from specific index
+
+def funa(arr,left,right):
+    if left >= right:
+        return
+    arr[left],arr[right] = arr[right],arr[left]
+    funa(arr,left+1,right-1)
+
+def reversarray(arr,left,right):
+    funa(arr,left,right)
+    return arr
+
+aa = reversarray([1,2,3,4,5,6,7,8,9,0],0,3)
+
+print(aa)
